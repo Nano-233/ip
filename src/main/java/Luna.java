0ffa90b4f4aebe7646195bcc88df9d62bd229672
Nano <86750728+Nano-233@users.noreply.github.com>
2025-01-22 11:01:38 +0800

    Renamed chatbot to Luna and added skeletal code
    - Added greeting and exit message and ASCII art for Luna.

---
+++ src/main/java/Luna.java
@@ -0,0 +1,12 @@
+public class Luna {
+  public static void main(String[] args) {
+    String line = "____________________________________________________________\n";
+    String logo = "     /\\_/\\  \n" +
+        "    ( o.o )  \n" +
+        "     > ❤ <    \n";
+    String start = "Hello I'm Luna \nHow may I help?\n";
+    String end = "Bye~\n";
+
+    System.out.println(line + logo + start + line + end + line);
+  }
+}
