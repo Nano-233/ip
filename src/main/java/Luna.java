fcb1d2dfd85f403fedf8fa9b8b68638cfa6c9144
Nano <86750728+Nano-233@users.noreply.github.com>
2025-01-22 11:32:30 +0800

    Added task management feature to Luna chatbot
    - Users can add tasks to a list.
    - Implemented "list" command to display all tasks.

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -9,19 +9,37 @@ public class Luna {
     String start = "Hello I'm Luna \nHow may I help?\n";
     String end = "Bye~\n";
 
+    String[] tasks = new String[100]; //array of tasks
+    int taskCount = 0;
+
     Scanner sc = new Scanner(System.in);
 
     System.out.print(line + logo + start + line); //start msg
 
     while (true) {
-      String input = sc.nextLine(); //reads input
+      String input = sc.nextLine().trim();
+
       if (input.equalsIgnoreCase("bye")) {
         break;
+      } else if (input.equalsIgnoreCase("list")) {
+        System.out.print(line);
+        if (taskCount == 0) {
+          System.out.println("Your list is empty.");
+        } else {
+          for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+          }
+        }
+        System.out.print(line);
+      } else {
+        tasks[taskCount] = input;
+        taskCount++;
+        System.out.print(line + "Added: \"" + input + "\"\n" + line);
       }
-      System.out.print(line + input + "\n" + line); //echo
     }
 
     System.out.print(line + end + line); //end msg
+    sc.close();
     System.exit(0);
   }
 }
