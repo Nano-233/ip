155c3b8dfb6feb2203553027821ef9cc55eec4c5
Nano <86750728+Nano-233@users.noreply.github.com>
2025-01-22 17:54:58 +0800

    Added functionality to mark and unmark tasks in Luna chatbot
    - Implemented the "mark <task_number>" command to mark tasks as done.
    - Implemented the "unmark <task_number>" command to mark tasks as not done.
    - Enhanced the task list to display completion status with [X] or [ ].
    - Ensured input validation for task numbers.

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -10,6 +10,7 @@ public class Luna {
     String end = "Bye~\n";
 
     String[] tasks = new String[100]; //array of tasks
+    boolean[] isDone = new boolean[100];
     int taskCount = 0;
 
     Scanner sc = new Scanner(System.in);
@@ -21,18 +22,41 @@ public class Luna {
 
       if (input.equalsIgnoreCase("bye")) {
         break;
+
       } else if (input.equalsIgnoreCase("list")) {
         System.out.print(line);
         if (taskCount == 0) {
           System.out.println("Your list is empty.");
         } else {
+          System.out.println("Here are the tasks in your list:");
           for (int i = 0; i < taskCount; i++) {
-            System.out.println((i + 1) + ". " + tasks[i]);
+            System.out.println((i + 1) + ". [" + (isDone[i] ? "X" : " ") + "] " + tasks[i]);
           }
         }
-        System.out.print(line);
+
+      } else if (input.startsWith("mark ")) {
+        int index = Integer.parseInt(input.split(" ")[1]) - 1;
+        if (index >= 0 && index < taskCount) {
+          isDone[index] = true;
+          System.out.print(line + "Nice! I've marked this task as done:\n");
+          System.out.println("[X] " + tasks[index] + "\n" + line);
+        } else {
+          System.out.print(line + "Invalid task number.\n" + line);
+        }
+
+      } else if (input.startsWith("unmark ")) {
+        int index = Integer.parseInt(input.split(" ")[1]) - 1;
+        if (index >= 0 && index < taskCount) {
+          isDone[index] = false;
+          System.out.print(line + "OK, I've marked this task as not done yet:\n");
+          System.out.println("[ ] " + tasks[index] + "\n" + line);
+        } else {
+          System.out.print(line + "Invalid task number.\n" + line);
+        }
+
       } else {
         tasks[taskCount] = input;
+        isDone[taskCount] = false;
         taskCount++;
         System.out.print(line + "Added: \"" + input + "\"\n" + line);
       }
